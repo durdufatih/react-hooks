24ed1ff5d8d480e538d330b7d75c398751bd65d5
durdufatih <mehmet-fatih.durdu@sovos.com>
2021-06-14 13:11:03 +0300

    Some css code has been added for minimize screen

--- src/components/UseContextComponent.js
+++ src/components/UseContextComponent.js
@@ -1,10 +1,14 @@
 import { useContext } from "react";
-import App from "../App";
 import NumberContext from "./NumberContext";
 
 function UseContextComponent() {
   const value = useContext(NumberContext);
-  return <div>This value is {value}</div>;
+  return (
+    <div>
+      <div>UseContext Example </div>
+      <div>This value is {value}</div>
+    </div>
+  );
 }
 
 export default UseContextComponent;
